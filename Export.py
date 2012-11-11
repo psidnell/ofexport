bb067a8868d7026cfcb38d49491ff38227bcbb34
Paul Sidnell <paul-adrian.sidnell@hp.com>
2012-11-11 11:33:30 +0000

    better

--- Export.py
+++ Export.py
@@ -56,12 +56,11 @@ CREATE INDEX Context_parent on Context (parent);
 
 '''
 TODO
-- tasklist->children
-- generic node class
 - link objects properly
 - sort tasks
 - visitor pattern
 - use select dateField(ZTIME, 'unixepoch', '+31 years') from ...
+- slots?
 '''
 
 THIRTY_ONE_YEARS = 60 * 60 * 24 * 365 * 31 + 60 * 60 * 24 * 8
@@ -75,7 +74,7 @@ class AttribDesc( object ):
         instance.__dict__[self._name] = value
         
 class TypedDesc(object):
-    def __init__(self,name, exptype):
+    def __init__(self,name, exptype, value=None):
         self.name = name
         self.expected_type = exptype
     def __get__(self,obj,cls):
@@ -84,46 +83,63 @@ class TypedDesc(object):
         else:
             return obj.__dict__[self.name]
     def __set__(self,obj,value):
-        if not isinstance(value,self.expected_type):
+        if not (isinstance(value,self.expected_type) or value == None):
             raise TypeError("Expected %s got %s" % (self.expected_type, value.__class__.__name__))
         obj.__dict__[self.name] = value
     def __delete__(self,obj):
         raise AttributeError("Can't delete")
 
 class Node (object):
-    name = TypedDesc ('name', unicode)
-    parent = None
     def __init__ (self, attribs):
         self.__dict__.update (attribs)
+        self.children = []
     def __getitem__ (self, key):
         return self.__dict__[key]
     def __setitem__ (self, key, val):
         self.__dict__[key] = val
     def __contains__ (self, key):
         return key in self.__dict__
-
+    def getChildNames (self):
+        return '[' + ','.join([child.name for child in self.children]) + ']'
 
 class Context(Node):
     TABLE='context'
     COLUMNS=['persistentIdentifier', 'name', 'parent', 'childrenCount']
+    name = TypedDesc ('name', unicode)
     def __init__(self, param):
         Node.__init__(self,param)
         self.name=self.__dict__['name']
+    def __str__ (self):
+        return "Context:" + self.name
 
 class Task(Node):
     TABLE='task'
     COLUMNS=['persistentIdentifier', 'name', 'dateDue', 'dateCompleted','projectInfo', 'context', 'containingProjectInfo', 'childrenCount', 'parent']
+    name = TypedDesc ('name', unicode)
     context = TypedDesc('context', Context)
+    #project = TypedDesc('project', Project) forward reference?
     def __init__(self, param):
         Node.__init__(self,param)
         self.name=self.__dict__['name']
+        self.parent = None
+    def __str__ (self):
+        parent_task = ''
+        if self.parent != None:
+            parent_task = ' parentTask:' + self.parent.name
+        return "Task:" + self.name + parent_task
 
 class Folder(Node):
     TABLE='folder'
     COLUMNS=['persistentIdentifier', 'name', 'childrenCount', 'parent']
+    name = TypedDesc ('name', unicode)
     def __init__(self, param):
         Node.__init__(self,param)
         self.name=self.__dict__['name']
+    def __str__ (self):
+        parent_folder = ''
+        if self.parent != None:
+            parent_folder = ' parentFolder:' + self.parent.name
+        return "Folder:" + self.name + parent_folder + ' ' + self.getChildNames()
         
 class ProjectInfo(Node):
     TABLE='projectinfo'
@@ -134,7 +150,13 @@ class ProjectInfo(Node):
 class Project(Task):
     project_info = TypedDesc ('project_info', ProjectInfo)
     folder = TypedDesc ('folder', Folder)
-
+    def __init__(self):
+        self.folder = None
+    def __str__ (self):
+        parent_folder = ''
+        if self.folder != None:
+            parent_folder = ' parentFolder:' + self.folder.name  
+        return "Project:" + self.name + parent_folder
     
 def query (conn, clazz):
     c = conn.cursor()
@@ -151,91 +173,69 @@ def query (conn, clazz):
     c.close()
     return results
 
-def transmute_projects (project_infos, folders, tasks):
+def transmute_projects (project_infos, tasks):
     '''
     Some tasks are actually projects, convert them
     '''
-    projects = []
+    projects = {}
     for project in tasks.values():        
         if project['projectInfo'] != None:
-            projects.append(project)
+            projects[project['persistentIdentifier']] = project
             project_info = project_infos[project['projectInfo']]
-            project_info.name = project.name
+            project.project_info = project_info
             project.__class__ = Project
-            
-            # Wire the project to it's folder while we're here 
-            if project_info['folder'] != None:
-                folder = folders[project_info['folder']]
-                project.folder = folder
-                if not ('taskList' in folder):
-                    folder['taskList'] = [project]
-                else:
-                    folder['taskList'].append(project)
+            project.__init__()
+            project_info.project = project
     return projects
 
-def wire_projects_and_folders (project_infos, folders, tasks):
+def wire_projects_and_folders (projects, folders):
     '''
     Some tasks are actually projects, convert them
     '''
-    for project in tasks.values():        
+    for project in projects.values():        
         if project['projectInfo'] != None:
-            project_info = project_infos[project['projectInfo']]
-            project_info.name = project.name
-            project.__class__ = Project
-            
-            # Wire the project to it's folder while we're here 
+            project_info = project.project_info
             if project_info['folder'] != None:
                 folder = folders[project_info['folder']]
                 project.folder = folder
-                if not ('taskList' in folder):
-                    folder['taskList'] = [project]
-                else:
-                    folder['taskList'].append(project)
+                folder.children.append(project)
 
 def wire_task_hierarchy (tasks):
     for task in tasks.values():  
         if task['parent'] != None:
+            pprint (task.__dict__)
             parent = tasks[task['parent']]
-            if not ('taskList' in parent):
-                parent['taskList'] = [task]
-            else:
-                parent['taskList'].append(task)
-
+            parent.children.append(task);
+            task.parent = parent
+            
 def wire_tasks_to_enclosing_projects (project_infos, tasks):
     for task in tasks.values():  
         if task['containingProjectInfo'] != None:
-            projectInfo = project_infos[task['containingProjectInfo']]
-            task['projectName'] = projectInfo['name']
+            project_info = project_infos[task['containingProjectInfo']]
+            project = project_info.project
+            task['projectName'] = project.name
+            task.project = project
        
 def wire_tasks_and_contexts (contexts, tasks):
     for task in tasks.values():  
-
         if task['context'] != None:
             context = contexts[task['context']]
             task.context = context
-            if not ('taskList' in context):
-                context['taskList'] = [task]
-            else:
-                context['taskList'].append(task)
+            context.children.append(task)
             task['contextName'] = context['name']
             
 def wire_folder_hierarchy (folders):
     for folder in folders.values():
         if folder['parent'] != None:
             parent = folders[folder['parent']]
-            if not ('taskList' in parent):
-                parent['taskList'] = [folder]
-            else:
-                parent['taskList'].append(folder)
+            parent.children.append (folder)
+            folder.parent = parent
                 
 def wire_context_hierarchy (contexts):
     for context in contexts.values():
         if context['parent'] != None:
             parent = contexts[context['parent']]
-            if not ('taskList' in parent):
-                parent['taskList'] = [context]
-            else:
-                parent['taskList'].append(context)
+            parent.children.append(context)
 
 def printTree (depth, item):
     dateCompleted = ''
@@ -247,10 +247,10 @@ def printTree (depth, item):
     project = ''
     if 'projectName' in item:
         project = item['projectName']
-    print (' ' * (depth * 4)) + item['name'] + '[' + str(item['childrenCount']) + '] ' + item.__class__.__name__ + ' P:' + project + ' C:' + context + ' ' + str(dateCompleted)
-    if 'taskList' in item:
-        for subItem in item['taskList']:
-            printTree(depth + 1, subItem)
+    print (' ' * (depth * 4)) + str(item)
+    print (' ' * (depth * 4)) + item['name'] + '[' + str(len(item.children)) + '] ' + item.__class__.__name__ + ' P:' + project + ' C:' + context + ' ' + str(dateCompleted)
+    for subItem in item.children:
+        printTree(depth + 1, subItem)
 
 def buildModel (db):
     conn = sqlite3.connect(db)
@@ -259,7 +259,8 @@ def buildModel (db):
     folders = query (conn, clazz=Folder)
     tasks = query (conn, clazz=Task)
     
-    projects = transmute_projects (project_infos, folders, tasks)
+    projects = transmute_projects (project_infos, tasks)
+    wire_projects_and_folders(projects, folders)
     wire_task_hierarchy(tasks)
     wire_tasks_to_enclosing_projects (project_infos, tasks)
     wire_tasks_and_contexts(contexts, tasks)
