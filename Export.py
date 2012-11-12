1861bbc4e1bdede6f03ba1b63f4f95769dd1d852
Paul Sidnell <paul-adrian.sidnell@hp.com>
2012-11-12 17:57:42 +0000

    Changes from datetime to date to simplify consequences of date maths rounding Tidied the report

--- Export.py
+++ Export.py
@@ -1,7 +1,5 @@
 import sqlite3
-from pprint import pprint
-import datetime
-import sys
+from datetime import date, timedelta
 
 '''
 sqlite3 ~/Library/Caches/com.omnigroup.OmniFocus/OmniFocusDatabase2
@@ -98,7 +96,7 @@ class DateAttribDesc (AttribDesc):
         val = AttribDesc.__get__(self, obj, cls)
         if val == None:
             return None
-        return datetime.datetime.fromtimestamp(THIRTY_ONE_YEARS + val)
+        return date.fromtimestamp(THIRTY_ONE_YEARS + val)
 
 class BoolAttribDesc (AttribDesc):
     def __init__( self, name ):
@@ -394,16 +392,16 @@ class PrintingVisitor(Visitor):
 DAYS={'0':'Sun', '1':'Mon', '2':'Tue', '3':'Wed', '4':'Thu', '5':'Fri', '6':'Sat'}
 MONTHS={'01':'Jan', '02':'Feb', '03':'Mar', '04':'Apr', '05':'May', '06':'Jun', '07':'Jul','08':'Aug', '09':'Sep', '10':'Oct', '11':'Nov', '12':'Dec'}
 
-def format_date (datetime = datetime.datetime.now()):
-        if datetime == None:
+def format_date (thedate = date.today()):
+        if thedate == None:
             return ''
-        result = DAYS[datetime.strftime('%w')]
-        result += '-' + MONTHS[datetime.strftime('%m')]
-        result += datetime.strftime('-%d-%Y')
+        result = DAYS[thedate.strftime('%w')]
+        result += '-' + MONTHS[thedate.strftime('%m')]
+        result += thedate.strftime('-%d-%Y')
         return result
     
-def format_timestamp (datetime = datetime.datetime.now()):
-        return datetime.strftime('%Y-%m-%d')
+def format_timestamp (thedate = date.today()):
+        return thedate.strftime('%Y-%m-%d')
     
 class WeeklyReportVisitor(Visitor):
 
@@ -414,7 +412,7 @@ class WeeklyReportVisitor(Visitor):
         self.proj_pfx = proj_pfx
     def end_project (self, project):
         if len(self.tasks) > 0 or self.completed_recently(project):
-            print >>self.out, self.proj_pfx + ' Project: ' + str(project)
+            print >>self.out, self.proj_pfx + ' ' + str(project)
             self.tasks.sort(key=lambda task:task.date_completed)
             for task in self.tasks:
                 print >>self.out, '- ' + str(task) + ' - ' + format_date(task.date_completed)
@@ -431,14 +429,14 @@ class WeeklyReportVisitor(Visitor):
         if task.date_completed == None:
             return False
         completed = task.date_completed
-        cutoff = datetime.datetime.now() - datetime.timedelta(self.days)
+        cutoff = date.today() - timedelta(self.days)
         if cutoff <= completed:
             return True
     
 
 folders, contexts = buildModel ('/Users/psidnell/Library/Caches/com.omnigroup.OmniFocus/OmniFocusDatabase2')
 
-weekly_report=open('/Users/psidnell/Documents/Reports/WeeklyReport-' + datetime.datetime.now().strftime('%W') + '.ft', 'w')
+weekly_report=open('/Users/psidnell/Documents/Reports/WeeklyReport-' + date.today().strftime('%W') + '.ft', 'w')
 print >>weekly_report, '# Weekly Progress Report'
 print >>weekly_report
 print >>weekly_report, '## Paul Sidnell ' + format_date()
