6669cf3501fb4874ff767a07aac06280007c18c0
Paul Sidnell <paul-adrian.sidnell@hp.com>
2012-11-13 20:20:12 +0000

    better report

--- linkgen.py
+++ linkgen.py
@@ -1,32 +1,34 @@
-from omnifocus import traverse_folder, traverse_contexts, build_model, Visitor
+from omnifocus import traverse_folders, traverse_contexts, build_model, Visitor
 import os
 import codecs
 
 class LinkGenVisitor(Visitor):
-    def __init__ (self, out, print_tasks, indent=4):
+    def __init__ (self, out, types = {'Folder', 'Project', 'Task'}, indent=4):
         self.depth = 0
         self.out = out
-        self.print_tasks = print_tasks
+        self.types = types
         self.indent = indent
     def begin_folder (self, folder):
-        self.print_link ('folder', folder)
+        if 'Folder' in self.types:
+            self.print_link ('folder', folder)
         self.depth+=1
     def end_folder (self, folder):
         self.depth-=1
     def begin_project (self, project):
-        if project.date_completed == None and self.print_tasks:
+        if project.date_completed == None and 'Project' in self.types:
             self.print_link ('task', project)
         self.depth+=1
     def end_project (self, project):
         self.depth-=1
     def begin_task (self, task):
-        if task.date_completed == None and self.print_tasks:
+        if task.date_completed == None and 'Task' in self.types:
             self.print_link ('task', task)
         self.depth+=1
     def end_task (self, task):
         self.depth-=1
     def begin_context (self, context):
-        self.print_link ('context', context)
+        if 'Context' in self.types:
+            self.print_link ('context', context)
         self.depth+=1
     def end_context (self, context):
         self.depth-=1
@@ -39,10 +41,17 @@ class LinkGenVisitor(Visitor):
 folders, contexts = build_model ('/Users/psidnell/Library/Caches/com.omnigroup.OmniFocus/OmniFocusDatabase2')
 
 file_name='/Users/psidnell/Documents/oflinks.html'
+
 out=codecs.open(file_name, 'w', 'utf-8')
-for folder in folders:
-    if folder.name == 'Work':
-        traverse_folder (LinkGenVisitor (out, True), folder)
-traverse_contexts (LinkGenVisitor (out, False), contexts)
+
+traverse_folders (LinkGenVisitor (out, types ={'Folder', 'Project'}), folders)
+print >>out, '<hr/>'
+
+traverse_contexts (LinkGenVisitor (out, types={'Context'}), contexts)
+print >>out, '<hr/>'
+
+traverse_folders (LinkGenVisitor (out, types ={'Folder','Project','Task'}), folders)
+
 out.close()
+
 os.system("open '" + file_name + "'")
